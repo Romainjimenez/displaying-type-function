195d7e62f2b2ec3bb0de041e9ff9b01f28a71b72
Romainjimenez <36897835+Romainjimenez@users.noreply.github.com>
2018-05-28 15:02:34 +0200

    Add files via upload

---
+++ displaying-type-function.php
@@ -0,0 +1,36 @@
+<?php
+
+  $TextTitle = "« Portez ce vieux whisky au juge blond qui fume. »";
+  $TextParagraph = "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.";
+
+echo '<h1>';
+echo $TextTitle;
+echo '<h1>';
+
+echo '<h2>';
+echo $TextTitle;
+echo '</h2>';
+
+echo '<h3>';
+echo $TextTitle;
+echo '</h3>';
+
+echo '<h4>';
+echo $TextTitle;
+echo '</h4>';
+
+echo '<h5>';
+echo $TextTitle;
+echo '</h5>';
+
+echo '<h6>';
+echo $TextTitle;
+echo '</h6>';
+
+
+echo '<p>';
+echo $TextParagraph;
+echo '</p>';
+
+
+?>
